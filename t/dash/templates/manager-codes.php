2c579e78cc8b3413a6816941c8081562f63b969b
Donghua.Lau <1485084328@qq.com>
2015-07-18 22:23:53 +0800

    fixed a bug

--- t/dash/templates/manager-codes.php
+++ t/dash/templates/manager-codes.php
@@ -12,7 +12,7 @@
 			//echo "page open first";
 			$cur_site = 'www.dashangcloud.com';
 			$cur_fee = 9.9;
-			$dskey = generate_dash_key(1,$cur_site,$cur_fee);
+			$dskey = generate_dash_key(get_current_user_id(),$cur_site,$cur_fee);
 		}else{
 			$cur_site = $_POST['site'];
 			$cur_fee = $_POST['fee'];
